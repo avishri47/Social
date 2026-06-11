78d3854942151ebd3961cf10fd326bf320c5e459
Avinash Shrivastava <ashrivastava191@gmail.com>
2026-06-11 23:59:16 +0530

    created global state for avatar updation

--- social-media-portal/src/context/AuthContext.jsx
+++ social-media-portal/src/context/AuthContext.jsx
@@ -2,8 +2,13 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext();
 
+
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const DEFAULT_AVATARS = {
+    male: 'https://cdn.vectorstock.com/i/1000v/54/69/male-user-icon-vector-8865469.jpg',     // High-quality free avatar API
+    female: 'https://cdn.vectorstock.com/i/1000v/44/13/grey-female-avatar-placeholder-vector-38594413.jpg',
+  };
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
 
@@ -18,11 +23,13 @@ export const AuthProvider = ({ children }) => {
   };
   const signout = () => {
     setUser(null);
-    localStorage.removeItem("user");
+localStorage.clear(); 
   };
-
+const avatarUrl = user?.customAvatar 
+    ? user.customAvatar 
+    : (DEFAULT_AVATARS[user?.gender?.toLowerCase()]);
   return (
-    <AuthContext.Provider value={{ user, signin, signout }}>
+    <AuthContext.Provider value={{ user, signin, signout, avatarUrl }}>
       {children}
     </AuthContext.Provider>
   );
